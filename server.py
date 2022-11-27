808dcd3bee2523670aa5f717c664b98de3acbb9e
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-27 13:45:41 -0600

    imported request

--- server.py
+++ server.py
@@ -1,4 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, FlaskForm
+from wtforms import TextField, BooleanField, TextAreaField, SubmitField
+import request
+
+class ContactForm(FlaskForm):
+    name = TextField("Name")
+    email = TextField("Email")
+    subject = TextField("Subject")
+    message = TextAreaField("Message")
+    submit = SubmitField("Send")
+
 
 app = Flask(__name__)
 
@@ -9,7 +19,12 @@ def home():
 
 @app.route('/contact.html')
 def contact():
-    return render_template('contact.html')
+    form = ContactForm()
+    if request.method == 'POST':
+        return 'Form posted.'
+    elif request.method == 'GET':
+        return render_template('contact.html,' form=form)
+
 
 @app.route('/pythonPortfolio.html')
 def python():
