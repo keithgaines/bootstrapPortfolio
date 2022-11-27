4356a471785d130d178432b75150c2e525a4fefb
Keith Gaines <patrick.keith.gaines@gmail.com>
2022-11-27 14:03:23 -0600

    commented out form

--- server.py
+++ server.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, FlaskForm
 from wtforms import TextField, BooleanField, TextAreaField, SubmitField
 import requests
 
-class ContactForm(FlaskForm):
-    name = TextField("Name")
-    email = TextField("Email")
-    subject = TextField("Subject")
-    message = TextAreaField("Message")
-    submit = SubmitField("Send")
+# class ContactForm(FlaskForm):
+#     name = TextField("Name")
+#     email = TextField("Email")
+#     subject = TextField("Subject")
+#     message = TextAreaField("Message")
+#     submit = SubmitField("Send")
 
 
 app = Flask(__name__)
@@ -19,10 +19,10 @@ def home():
 
 @app.route('/contact.html')
 def contact():
-    form = ContactForm()
-    if requests.method == 'POST':
-        return 'Form posted.'
-    elif requests.method == 'GET':
+    # form = ContactForm()
+    # if requests.method == 'POST':
+    #     return 'Form posted.'
+    if requests.method == 'GET':
         return render_template('contact.html,' form=form)
 
 
